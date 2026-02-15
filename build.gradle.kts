b2fea88eda978c15c06ba2f2b087f41bf36c2dd0
nuyuls79 <nuyulshare001@gmail.com>
2026-02-15 22:33:51 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.4.2") 
+        classpath("com.android.tools.build:gradle:8.7") 
         classpath("com.github.recloudstream:gradle:cce1b8d84d")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
